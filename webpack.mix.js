2e01cfffca81bd753199a44c5085536f39c8ca65
ignacio-thfoot <70500752+ignacio-thfoot@users.noreply.github.com>
2021-10-05 11:06:07 +0200

    update webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,23 @@
 const mix = require('laravel-mix');
+const del = require('del');
+require('laravel-mix-serve');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+mix.setPublicPath('./public');
+del('public/css/*.css');
+del('public/js/*.js');
+mix.js('resources/js/app.js', 'public/js').vue();
+mix.sass('resources/scss/entries/app.scss', 'public/css');
+
+mix.browserSync({
+    'port': 8000,
+	files: [
+		'resources/views',
+		'resources/assets',
+		'app',
+        'resources/css',
+        'resources/js'
+	],
+});
+
+mix.serve(); 
